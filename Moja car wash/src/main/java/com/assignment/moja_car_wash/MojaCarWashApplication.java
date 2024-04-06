0d6a35063ad8033bd7fe9f3d07a3383f454a3caa
Ladzi031 <ladziladzani@gmail.com>
2024-04-06 20:28:27 +0200

    first commit

---
+++ Moja car wash/src/main/java/com/assignment/moja_car_wash/MojaCarWashApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.moja_car_wash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojaCarWashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojaCarWashApplication.class, args);
+	}
+
+}
---
+++ Moja car wash/src/test/java/com/assignment/moja_car_wash/MojaCarWashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.moja_car_wash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojaCarWashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
